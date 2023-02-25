29a63028e9161349bbf6bd91fa1ff4da5cdd72e0
s3bbar <seba.baranowski@gmail.com>
2023-02-25 20:34:17 +0100

    sprawdzanie po zajeciach

--- selenium_5.py
+++ selenium_5.py
@@ -1,5 +1,31 @@
 #Page Object Model
+from selenium import webdriver
 
 class LoginPage:
     def __int__(self, driver):
         self.driver = driver
+        self.username_field_id = 'user-name'
+        self.password_field_id = 'password'
+        self.login_button_name = 'login-button'
+
+    def open(self):
+        self.driver.get('http://www.saucedemo.com')
+
+    def enter_username(self, username):
+        field = self.driver.find_element('id', self.username_field_id)
+        field.clear()
+        field.send_keys(username)
+
+    def enter_password(self, password):
+        field = self.driver.find_element('id', self.password_field_id)
+        field.clear()
+        field.send_keys(password)
+
+    def click_login(self):
+        button = self.driver_find_element('name', self.login_button_name)
+        button.click()
+
+
+
+driver = webdriver.Chrome()
+page1 = LoginPage(driver)
